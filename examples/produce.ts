f3701455e3831fcbc80a104e2c0265b05aed82be
Elton Okawa <elton.okawa@gmail.com>
2024-04-21 07:37:06 -0300

    fix: local execution

--- examples/produce.ts
+++ examples/produce.ts
@@ -1,13 +1,15 @@
-import { Kafka } from 'kafkajs';
+import { Kafka, Partitioners } from 'kafkajs';
 import { TOPIC } from '../src/app.controller';
 
 async function main() {
   const kafka = new Kafka({
     clientId: 'producer',
-    brokers: ['localhost:9092'],
+    brokers: ['localhost:9095'],
   });
 
-  const producer = kafka.producer();
+  const producer = kafka.producer({
+    createPartitioner: Partitioners.DefaultPartitioner,
+  });
   await producer.connect();
 
   await producer.send({
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { Partitioners } from 'kafkajs';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -9,11 +10,14 @@ async function bootstrap() {
       transport: Transport.KAFKA,
       options: {
         client: {
-          brokers: ['localhost:9094'],
+          brokers: ['localhost:9095'],
         },
         consumer: {
           groupId: 'group-id',
           allowAutoTopicCreation: true,
+        },
+        producer: {
+          createPartitioner: Partitioners.DefaultPartitioner,
         },
       },
     },
